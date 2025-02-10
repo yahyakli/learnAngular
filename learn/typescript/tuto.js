ecae9df5e7b627fbaa92c70f38944a9b97574115
yahya akli <akli.yahya05@gmail.com>
2025-02-10 18:13:58 +0100

    new

---
+++ learn/typescript/tuto.js
@@ -0,0 +1,6 @@
+"use strict";
+const myfunction = (a, b) => {
+    return a + b;
+};
+console.log(myfunction('yahya', 'akli'));
+console.log(myfunction('5', "10"));
---
+++ learn/typescript/tuto.ts
@@ -0,0 +1,6 @@
+const myfunction = (a: string, b: string): string => {
+    return a + b;
+}
+
+console.log(myfunction('yahya', 'akli'));
+console.log(myfunction('5', "10"));
