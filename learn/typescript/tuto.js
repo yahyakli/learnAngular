357e1b1b4f9a2b4fe27b025d7684a2bd445ac642
yahya akli <akli.yahya05@gmail.com>
2025-02-11 11:24:17 +0100

    new

--- learn/typescript/tuto.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const signContract = (employee) => {
-    console.log('contract signed by ' + employee.name + ' with email: ' + employee.email);
-};
-signContract({ name: 'yahya', creditStore: 20, id: 1, email: 'yahya@gmail.com' });
