8775cfb161101b2167126fa3b6a81fa062345d57
yahya akli <akli.yahya05@gmail.com>
2025-02-11 13:03:19 +0100

    new

---
+++ first-ng-app/src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { GreetingComponent } from '../components/greeting/greeting.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [GreetingComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  message = signal("Hello, World!");
+}
