4f4945348243c864f746572038d60eea992e7e34
yahya akli <akli.yahya05@gmail.com>
2025-02-11 20:46:01 +0100

    new

--- first-ng-app/src/app/services/todos.service.ts
+++ first-ng-app/src/app/services/todos.service.ts
@@ -1,29 +1,14 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Todo } from '../model/todo.type';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodosService {
-  todoItems: Array<Todo> = [
-    {
-      title: 'test',
-      id: 0,
-      userId: 1,
-      completed: false
-    },
-    {
-      title: 'test',
-      id: 1,
-      userId: 4,
-      completed: true
-    },
-    {
-      title: 'test',
-      id: 2,
-      userId: 5,
-      completed: false
-    },
-  ]
-  constructor() { }
+  http = inject(HttpClient);
+  getTodos() {
+    const url = "https://jsonplaceholder.typicode.com/todos";
+    return this.http.get<Array<Todo>>(url);
+  }
 }
--- first-ng-app/src/app/todos/todos.component.ts
+++ first-ng-app/src/app/todos/todos.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { TodosService } from '../services/todos.service';
 import { Todo } from '../model/todo.type';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-todos',
@@ -13,7 +14,13 @@ export class TodosComponent implements OnInit {
   todoItems = signal<Array<Todo>>([]);
 
   ngOnInit(): void {
-    console.log(this.todoService.todoItems);
-    this.todoItems.set(this.todoService.todoItems);
+    this.todoService.getTodos().pipe(
+      catchError(err => {
+        console.log(err);
+        throw err;
+      })
+    ).subscribe(todos => {
+      this.todoItems.set(todos);
+    });
   }
 }
